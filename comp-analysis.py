b94e94fbb10594a92c8eb95ff080cdb656787e75
Yitao Li <yitao@rstudio.com>
2021-12-08 16:15:55 +0000

    update comment and other minor changes
    Signed-off-by: Yitao Li <yitao@rstudio.com>

--- comp-analysis.py
+++ comp-analysis.py
@@ -5,11 +5,11 @@ import pandas as pd
 import requests
 
 resp = requests.get("https://www.levels.fyi/js/salaryData.json")
-print("Response code: ", resp.status_code)
+print(f"Response: {resp.status_code}")
 if resp.status_code != HTTPStatus.OK:
     # AFAIK this endpoint only responds with HTTP 200 and not any other 2xx
     # status for successful queries
-    raise RuntimeError("Failed to fetch data!")
+    raise RuntimeError(f"Possible failure in fetching salaryData.json: {resp.status_code}")
 
 data = resp.json()
 df = pd.DataFrame(data)
@@ -20,11 +20,16 @@ df = df[ \
   (pd.to_numeric(df["yearsofexperience"]) <= 8) \
 ]
 
-print("Num samples:", df.shape[0])
+print(f"Num samples: {df.shape[0]}")
 # All monetary quantities are stored in USD in levels.fyi data.
-print("Median:", pd.DataFrame.median(pd.to_numeric(df.totalyearlycompensation)), "k USD")
-print("Standard deviation:", pd.DataFrame.std(pd.to_numeric(df.totalyearlycompensation)), "k USD")
-p90 = pd.to_numeric(df.totalyearlycompensation).quantile(q = 0.9)
-print("90-th percentile:", p90, "k USD")
+print(f"Median: {pd.DataFrame.median(pd.to_numeric(df.totalyearlycompensation))} k USD")
+print(f"Standard deviation: {pd.DataFrame.std(pd.to_numeric(df.totalyearlycompensation))} k USD")
+
 kUsdToCad = 1.28
-print("90-th percentile:", p90 * kUsdToCad, "k CAD")
+
+p90 = pd.to_numeric(df.totalyearlycompensation).quantile(q = 0.9)
+print(f"90-th percentile: {p90} k USD")
+print(f"90-th percentile: {p90 * kUsdToCad} k CAD")
+
+p75 = pd.to_numeric(df.totalyearlycompensation).quantile(q = 0.75)
+print(f"75-th percentile: {p75 * kUsdToCad} k CAD")
